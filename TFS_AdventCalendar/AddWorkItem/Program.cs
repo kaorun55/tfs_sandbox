bd35d9eb66cafa80a65f9c4357ca23fb785febca
kaorun55 <kaorun55@gmail.com>
2011-12-05 16:16:24 +0900

    AdventCalendarの1回目のソースを追加

---
+++ TFS_AdventCalendar/AddWorkItem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using TfsAdventCalendar;
+
+namespace AddWorkItem
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            try {
+                Uri uri = new Uri( "http://win7-vm:8080/tfs/" );
+                NetworkCredential credential = new NetworkCredential( "hoge", "foo" );
+                string collenctionName = "win7-vm\\DefaultCollection";
+                string projectName = "TFS_API_SAMPLE";
+
+                TfsClient tfs = new TfsClient( uri, credential,
+                                    collenctionName, projectName );
+                tfs.AddWorkItem();
+
+                Console.WriteLine( "Success" );
+            }
+            catch ( Exception ex ) {
+                Console.WriteLine( "Error : " + ex.Message );
+            }
+        }
+    }
+}
